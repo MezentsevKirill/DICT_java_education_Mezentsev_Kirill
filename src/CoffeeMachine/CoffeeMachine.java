600e4a625e982a0a2f2c90b094eae78075050ccf
MezentsevKirill <k.v.mezentsev@student.khai.edu>
2022-11-26 19:18:30 +0200

    CoffeeMachine 4-th stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,39 +1,102 @@
 package CoffeeMachine;
 import java.util.Scanner;
 
+
+
 public class CoffeeMachine {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int water = 200;
-        int milk = 50;
-        int coffeeBeans = 15;
+    public static int [] numbers = {400, 540, 120, 9, 550};
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void printBoard() {
+        System.out.println("\nThe coffee machine has:\n" +
+                numbers[0] + " of water\n" +
+                numbers[1] + " of milk\n" +
+                numbers[2] + " of coffee beans\n" +
+                numbers[3] + " of disposable cups\n" +
+                numbers[4] + " of money");
+    }
+
+    public static int [] espresso (){
+         numbers[0] -= 250;
+         numbers[2] -= 16;
+         numbers[3] -= 1;
+         numbers[4] -= 4;
+         return numbers;
+    }
+
+    public static int [] latte (){
+        numbers[0] -= 350;
+        numbers[1] -= 75;
+        numbers[2] -= 20;
+        numbers[3] -= 1;
+        numbers[4] -= 7;
+        return numbers;
+    }
+
+    public static int [] cappuccino () {
+        numbers[0] -= 200;
+        numbers[1] -= 100;
+        numbers[2] -= 12;
+        numbers[3] -= 1;
+        numbers[4] -= 6;
+        return numbers;
+
+    }
+
+    public static void buy() {
+        System.out.print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n> ");
+        int userChoice = scanner.nextInt();
+        if (userChoice == 1) {
+            numbers = espresso();
+
+        } else if (userChoice == 2) {
+            numbers = latte();
+
+        } else if (userChoice == 3) {
+            numbers = cappuccino();
+
+        }
+
+
+    }
+
+    public static void fill() {
         System.out.print("Write how many ml of water the coffee machine has:\n> ");
         int userWater = scanner.nextInt();
         System.out.print("Write how many ml of milk the coffee machine has:\n> ");
         int userMilk = scanner.nextInt();
         System.out.print("Write how many grams of coffee beans the coffee machine has:\n> ");
         int userCoffeeBeans = scanner.nextInt();
-        System.out.print("Write how many cups of coffee you will need:\n> ");
-        int userCups = scanner.nextInt();
+        System.out.print("Write how many disposable coffee cups you will need:\n> ");
+        int userDisposableCups = scanner.nextInt();
+        numbers[0] += userWater;
+        numbers[1] += userMilk;
+        numbers[2] += userCoffeeBeans;
+        numbers[3] += userDisposableCups;
+    }
 
-        if (userWater >= userCups * water && userMilk >= userCups * milk && userCoffeeBeans >= userCups *
-                coffeeBeans) {
-            int moreCups = (userWater + userMilk + userCoffeeBeans) / (userCups * water + userCups * milk +
-                    userCups * coffeeBeans);
-            if (moreCups - userCups == 0) {
-                System.out.println("Yes, I can make that amount of coffee");
-            } else {
-                System.out.println("Yes, I can make that amount of coffee (and even " + (moreCups - userCups) +
-                        " more than that)");
+    public static void take() {
+        System.out.println("I gave you " + numbers[4]);
+        numbers[4] -= numbers[4];
+    }
 
-            }
+    public static void main(String[] args) {
+        printBoard();
+        System.out.print("Write action (buy, fill, take):\n> ");
+        String userAction = scanner.next();
+        if (userAction.equals("buy")) {
+            buy();
+
+        } else if (userAction.equals("fill")) {
+            fill();
+
+        } else if (userAction.equals("take")) {
+            take();
+
         }
 
-        if (userWater < userCups * water || userMilk < userCups * milk || userCoffeeBeans < userCups *
-                coffeeBeans) {
-            int moreCups = (userWater + userMilk + userCoffeeBeans) / (water + milk + coffeeBeans);
-            System.out.println("No, I can make only " + (moreCups) + " cups of coffee");
-        }
+        printBoard();
+
 
 
     }
